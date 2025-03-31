b3fc828947dbbbb4891dd9fad200b1ac79fd6db7
dominus10 <38601700+dominus10@users.noreply.github.com>
2025-03-31 17:09:47 +0900

    Delete dist directory

--- dist/packages/react/src/App.d.ts
+++
@@ -1,6 +0,0 @@
-import "@ez-calendar/core";
-declare const DatePicker: ({ value, onDateChange }: {
-    value: any;
-    onDateChange: any;
-}) => import("react").JSX.Element;
-export default DatePicker;
